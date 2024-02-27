6b5bb8854ec19fc0807cdae7ff3b74a835449846
Michael H. Roberts <mhr320@gmail.com>
2024-02-27 13:08:15 -0500

    minor

---
+++ private/views/faq_edit.view.php
@@ -0,0 +1,24 @@
+<?php $this->view('includes/header')?>
+<?php $this->view('includes/nav')?>
+<div class="container-fluid" style="margin-top: 60px;">
+
+<?php if(Auth::isAdmin() || Auth::isSuper()): ?>
+<h1 class="text-center">Edit FAQ Question</h1>
+			<div class="container-fluid" style="width: 600px;">
+				<form method="post">
+					<div class="mb-2">
+						<label for="faq" class="form-label">Edit FAQ</label>
+						<input value="<?=$edit_data->question?>" class="form-control" type="text" id="faq" name="faq" maxlength="700">
+						<label for="faq" class="form-label">Edit FAQ Answer</label>
+						<input value="<?=$edit_data->answer?>" class="form-control" type="text" id="faqa" name="faqa" maxlength="700">
+					</div>
+						<button type="submit" class="btn btn-primary float-end">Update</button>
+				</form>
+				<a class="link-light" href="<?=ROOT?>/faq">
+				<button class="btn btn-dark">Return To FAQs</button>
+				<a>
+			</div>
+		<?php endif; ?>
+</div>
+
+<?php $this->view('includes/footer')?>
