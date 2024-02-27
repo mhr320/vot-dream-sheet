c2df0912f21f129adcc17b9ab2e2a9c6642c284d
Michael H. Roberts <mhr320@gmail.com>
2024-02-26 21:32:49 -0500

    Very minor change to homeview

--- private/views/includes/header.view.php
+++ private/views/includes/header.view.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>To The Numbers</title>
+	<title>VOT Dream Sheet</title>
 	<link rel="stylesheet" type="text/css" href="<?=ASSETS?>/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="<?=ASSETS?>/all.min.css">
 	<script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
