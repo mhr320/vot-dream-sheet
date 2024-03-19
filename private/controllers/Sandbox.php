b49a8f8091c032fcc15dc5dd251923e192530003
Michael H. Roberts <mhr320@gmail.com>
2024-03-18 22:15:31 -0400

    the gd vot_pp display

--- private/controllers/Sandbox.php
+++ private/controllers/Sandbox.php
@@ -7,7 +7,16 @@ class Sandbox extends Controller
 	
 	function index()
 	{
+		$vot = new Vot_model();
+
+		$rows = $vot->findAll();
+
+		$pay_periods = get_payperiods('17 Dec 2023', '30 Dec 2023');
 		
-		echo $this->view('sandbox');
+
+		echo $this->view('sandbox',[
+			'rows'	=> $rows,
+			'pay_pers' 	=> $pay_periods,
+		]);
 	}
 }
--- private/views/sandbox.view.php
+++ private/views/sandbox.view.php
@@ -1,11 +1,60 @@
 <?php $this->view('includes/header')?>
+
 <div class="container-fluid">
+
 	<h1 class="text-center">Sandbox</h1>
+
 </div>
 
+<div class="container-fluid">
 
+	<?php 
 
+		function sandboxVolunteerShiftByDay($vol = array()) {
+			$arr 		= [];
+			$shifts 	= [];
+			$try 		= [];
 
+			foreach ( $vol as $k => $v ) {
 
+				$vdate 	= $v['date'] ?? "";
+				$vshift 	= $v['shift'] ?? "";
+				$vois 	= $v['ois'] ?? "";
+
+				// @$arr[$vdate] .= $vshift."-".$vois.",";	//@ suppresses the errror undefined array type
+
+				$arr[$vdate] = ["AM"=>"", "PM"=>"", "M"=>""];
+
+				if($vshift == 'am') {
+					$arr[$vdate] .=  ['AM'=> $vois.','];
+				}
+
+			}
+
+			// foreach( $arr as $day => $vols ) {
+
+			// 	$shiftOis 			= explode('-', $vols);
+			// 	$shft 				= $shiftOis[0];
+			// 	$ois 					= $shiftOis[1];
+			// 	$try[]					= $shft .$ois.',';
+			// 	$shifts[$day] 	= $try;
+			// }
+
+			// return $shifts;
+			return $arr;
+		}
+
+		$payPeriod 	= 	'pp9';
+		$vol 				= 	getVolDays($pay_pers, $rows, $payPeriod); 
+		$pp 				= 	explode('p',$payPeriod);
+		$volunteers 	= sandboxVolunteerShiftByDay($vol);
+
+		show($volunteers);
+		show($vol);
+
+	?>
+
+	
+</div>
 
 <?php $this->view('includes/footer')?>
